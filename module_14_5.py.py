a862a417091ae189c51e4ca7612444883b4a1ff5
TommyGun10 <161822959+TommyGun10@users.noreply.github.com>
2024-08-18 16:47:29 +0700

    Update module_14_5.py.py

--- module_14_5.py.py
+++ module_14_5.py.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 import crud_functions
 
-api = "7429366363:AAHxWZzj-M-sypwgulHAZ4i-rQWGpUGcg7w"
+api = "YOUR API"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
